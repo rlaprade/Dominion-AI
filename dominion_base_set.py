50f283aa50c63d7b65b90d666333c672f87e5461
rlaprade <rudylaprade@gmail.com>
2024-01-01 14:32:21 -0800

    Add festival and woodcutter card definitions

--- dominion_base_set.py
+++ dominion_base_set.py
@@ -1,4 +1,6 @@
 from card_classes import *
 
 chapel = Action("chapel", 2, effect = lambda player: player.voluntary_trash(4))
+festival = Action("festival", 5, plus_money=2, plus_actions=2, plus_buys=1)
 smithy = Action("smithy", 4, plus_cards=3)
+woodcutter = Action("woodcutter", 3, plus_money=2, plus_buys=1)
